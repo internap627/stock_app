d3bd4c6a5d42f3066b86d637fd61aee8f5dfe9b7
internap627 <you@example.com>
2018-10-23 14:40:02 +0100

    signup login logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :companies
   resources :brokers
   resources :users
+  post "/logout", to: "sessions#destroy"
+
   root "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
