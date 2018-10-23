46c400bf30e2d63d723aa2c0d56639006887fbe7
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-23 13:41:44 +0100

    added user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+   if params[:user][:password] == params[:user][:password_confirmation]
+     user = User.create(name: params[:user][:name], username: params[:user][:username], password: params[:user][:password])
+     byebug
+     session[:user_id] = user.id
+     redirect_to brokers_path
+   else
+     redirect_to new_user_path
+   end
+ end
+
 end
