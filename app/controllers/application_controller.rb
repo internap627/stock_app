e0069064ca1ee0228f21b89ddfac90dbc948f596
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-24 14:01:50 +0100

    added login verfication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :user
+  helper_method :current_user, :logged_in?, :user, :authorized?
   def user
     if session[:user_id] != nil
       user = User.find(session[:user_id])
@@ -8,6 +9,26 @@ class ApplicationController < ActionController::Base
       else
         user
       end
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      User.new
+    end
+  end
+
+  def logged_in?
+    !!current_user.id
+  end
+
+  def authorized?
+    if !logged_in?
+      flash[:authorized] = "Please log in to be able to view this page"
+      redirect_to login_path and return
+
     end
   end
 end
