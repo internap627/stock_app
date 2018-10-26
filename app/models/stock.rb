6434342ab64e417f96f5b3354939e363cd7581ab
internap627 <you@example.com>
2018-10-26 11:49:42 +0100

    stock ratio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,9 @@ class Stock < ApplicationRecord
   has_many :portfolios
   has_many :brokers, through: :portfolios
 
-  
+  def roe
+    stock = stocks.push(StockQuote::Stock.quote(self.symbol))
+    stock.pe_ratio
+  end
 
 end
