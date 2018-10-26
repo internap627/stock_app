5e01c9e38f6b93c2853abcd301f8428e4fc8da8a
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-26 14:25:34 +0100

    passwords must match

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,19 @@ class UsersController < ApplicationController
 
 
   def create
-    @user = User.new(name: params[:user][:name], username: params[:user][:username], password: params[:user][:password], broker_id: params[:user][:broker_id])
 
-    if @user.valid?
-       user = User.create(name: params[:user][:name], username: params[:user][:username], password: params[:user][:password], broker_id: params[:user][:broker_id])
-       session[:user_id] = user.id
-       redirect_to brokers_path
-     else
-       flash[:errors] = @user.errors.full_messages
-       render :new
-       #redirect_to new_user_path
-     end
+      @user = User.new(name: params[:user][:name], username: params[:user][:username], password: params[:user][:password], broker_id: params[:user][:broker_id], password_confirmation: params[:user][:password_confirmation])
+      if @user.valid?
+         @user.save
+         session[:user_id] = @user.id
+         redirect_to brokers_path
+       else
+         flash[:errors] = @user.errors.full_messages
+         render :new
+         #redirect_to new_user_path
+       end
+   end
 
- end
 
  def update
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,15 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates_confirmation_of :name, :username, :password
+  validates_confirmation_of :name, :username
   validates :username, uniqueness: true
   validates :password, length: { in: 6..15 }, on: :create
-  #validate :password_match
-  # validates :password,
-  #         # you only need presence on create
-  #         :presence => { :on => :create },
-  #         # allow_nil for length (presence will handle it on create)
-  #         :length   => { :minimum => 3, :allow_nil => true },
-  #         # and use confirmation to ensure they always match
-  #         :confirmation => true
+  validate :password_val
 
-  # def password_match
-  #   byebug
-  #   if password != password_confirmation
-  #     errors.add(:password, "and Password Confirmation must match")
-  #   end
-  # end
 
+  def password_val
+    password_confirmation != password
+  end
 
   def users_shares
     self.broker.stocks.map { |s| s.name}
